b396f4959514c6254394421018a696905e359a35
Dirk Friedenberger <Dirk.Friedenberger@deutschebahn.com>
2026-08-18 16:50:05 +0200

    some fittings

--- sysml2rdf/create_traceability_rdf_model.py
+++ sysml2rdf/create_traceability_rdf_model.py
@@ -8,11 +8,16 @@ def create_design_element(wrapper, design_element):
     if "traceability_id" not in design_element:
         return
     rdf_id = wrapper.add_labeled_instance(Traceability.DesignElement, design_element["name"], design_element['traceability_id'])
+    wrapper.add_str_property(Traceability.identifier, rdf_id, design_element["traceability_id"])
 
     if "traceability_ref" in design_element:
         for ref in design_element["traceability_ref"]:
             rdf_ref = wrapper.create_ref(Traceability.Requirement, ref)
             wrapper.add_reference(Traceability.satisfies, rdf_id, rdf_ref)
+
+    if "comment" in design_element:
+        wrapper.add_comment(rdf_id, "\n".join(design_element["comment"]))
+
 
 def create_traceability_rdf_model(collector: SysMLCollector):
     # Create RDF model
@@ -21,7 +26,7 @@ def create_traceability_rdf_model(collector: SysMLCollector):
     # Bind a user-declared namespace to a prefix
     graph.bind("trc", Traceability)
 
-    wrapper = GraphWrapper(graph, "http://frittenburger.de/sysml2rdf")
+    wrapper = GraphWrapper(graph, "https://osm.hpi.de/2026/08/SQuIRRL")
 
     # Add DesignElements and References to Requierments and Implemenations
     for clazz_id, clazz in collector.clazzes().items():
